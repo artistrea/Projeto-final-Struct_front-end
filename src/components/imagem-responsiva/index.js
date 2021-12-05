8f3100008939e278a11e5a624ef85a6a5b4bc7a5
artistrea <89547366+artistrea@users.noreply.github.com>
2021-12-05 19:11:08 -0300

    Revert "Revert "024_mais-2-componentes-e-algumas-mudanças-de-responsividade""

---
+++ src/components/imagem-responsiva/index.js
@@ -0,0 +1,11 @@
+import { Container } from "./styles"
+
+const ImagemR = () => {
+    return (
+        <Container>
+            <img class="foto" src="https://saborclub.uol.com.br/media/_versions/capa_quartourao_alta_credtomasrangel_horizontal_fixed_large.jpg" alt="Imagem responsiva"></img>
+        </Container>
+    )
+}
+
+export default ImagemR
