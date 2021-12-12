6943fabc2d191cd5aedd76885e5bf9a3b55146ed
LuigiPWOliveira <lpw.contato@gmail.com>
2021-12-12 12:11:51 -0300

    add link e botao sair do componente info-usuario, sem responsividade

--- src/components/info-usuario/index.js
+++ src/components/info-usuario/index.js
@@ -1,5 +1,8 @@
+import Cookies from "js-cookie"
 import { useUserContext } from "../../context/useUserContext"
+import Button from "../button"
 import { Container, Grid } from "./styles"
+
 
 const Info = () => {
 
@@ -10,6 +13,8 @@ const Info = () => {
             <Container>
                 <p>Nome do usuário</p>
                 <p>Email do usuário</p>
+                {/*<Link to="/formUsuario">Alterar meus dados</Link>*/}
+                <Button text="Sair" onClick={Cookies.remove("loggedinuser")}/>
             </Container>
         </>
         
--- src/pages/Usuario/styles.js
+++ src/pages/Usuario/styles.js
@@ -1,4 +1,13 @@
 import styled from "styled-components";
+import LoginBackground from "./../../imgs/LoginBackground.jpg"
 
 export const Container = styled.div`
+    display: flex;
+    height: 100%;
+    width: 100%;
+    background: url(${LoginBackground});
+    background-size: cover;
+    background-position: center;
+    
+    
 `;
